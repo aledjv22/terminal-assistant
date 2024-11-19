46264f6f399b7dbb3a35731d60ca2b94c95c8ee7
Victor Alejandro Díaz Jáuregui <victor.diaz.jauregui@mi.unc.edu.ar>
2024-11-18 21:46:51 -0300

    main.js formateado

--- main.js
+++ main.js
@@ -1,22 +1,20 @@
-import { config } from 'dotenv';
-import { HfInference } from '@huggingface/inference';
+import { config } from "dotenv";
+import { HfInference } from "@huggingface/inference";
 
 config();
 
 const hf = new HfInference(process.env.HUGGING_FACE_TOKEN);
-const message = 'Cuál es la capital de Argentina?';
+const message = "Cuál es la capital de Argentina?";
 
 const generateTextFromInput = async (message) => {
-    const response = await hf.chatCompletion({
-        model: 'mistralai/Mixtral-8x7B-Instruct-v0.1',
-        messages: [
-            { role: 'user', content: message }
-        ],
-        max_tokens: 1000,
-        temperature: 0.1,
-        seed: 42,
-    });
-    console.log(response.choices[0].message.content);
+  const response = await hf.chatCompletion({
+    model: "mistralai/Mixtral-8x7B-Instruct-v0.1",
+    messages: [{ role: "user", content: message }],
+    max_tokens: 1000,
+    temperature: 0.1,
+    seed: 42,
+  });
+  console.log(response.choices[0].message.content);
 };
 
 generateTextFromInput(message);
