d85bec6c5aea2c377ba18bb7b68de625a2fad584
Victor Alejandro Díaz Jáuregui <victor.diaz.jauregui@mi.unc.edu.ar>
2024-11-19 22:50:58 -0300

    formateo de main

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-import loading from "loading-cli";
-import readline from "readline";
-import { config } from "dotenv";
-import { HfInference } from "@huggingface/inference";
+import loading from 'loading-cli';
+import readline from 'readline';
+import { config } from 'dotenv';
+import { HfInference } from '@huggingface/inference';
 
 config();
 
@@ -13,22 +13,22 @@ const rl = readline.createInterface({
 });
 
 const generateTextFromInput = async () => {
-  rl.question("Tu: ", async (input) => {
+  rl.question('Tu: ', async (input) => {
     const load = loading({
-      text: "Pensando...",
-      frames: ["🌑", "🌒", "🌓", "🌔", "🌕", "🌖", "🌗", "🌘"],
+      text: 'Pensando...',
+      frames: ['🌑', '🌒', '🌓', '🌔', '🌕', '🌖', '🌗', '🌘'],
     }).start();
 
     const response = await hf.chatCompletion({
-      model: "mistralai/Mixtral-8x7B-Instruct-v0.1",
-      messages: [{ role: "user", content: input }],
+      model: 'mistralai/Mixtral-8x7B-Instruct-v0.1',
+      messages: [{ role: 'user', content: input }],
       max_tokens: 1000,
       temperature: 0.1,
       seed: 42,
     });
 
     load.stop();
-    console.log("\nAssistant:", response.choices[0].message.content);
+    console.log('\nAssistant:', response.choices[0].message.content);
     rl.close();
   });
 };
