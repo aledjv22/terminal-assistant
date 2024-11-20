3a8170fd5b07f14f5558aa41d2cf57e35d9e9b1a
Victor Alejandro Díaz Jáuregui <victor.diaz.jauregui@mi.unc.edu.ar>
2024-11-19 22:50:05 -0300

    actualizado del prettier

--- prettier.config.js
+++ prettier.config.js
@@ -1,3 +1,19 @@
 export const singleQuote = true;
 export const semi = true;
 export const tabWidth = 2;
+export const printWidth = 80;
+export const trailingComma = 'all';
+export const bracketSpacing = true;
+export const endOfLine = 'lf';
+export const arrowParens = 'always';
+
+export default {
+  singleQuote,
+  semi,
+  tabWidth,
+  printWidth,
+  trailingComma,
+  bracketSpacing,
+  endOfLine,
+  arrowParens,
+};
