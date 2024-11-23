46d4c872f949858edaa365e4d2ced2bb267732d2
Victor Alejandro Díaz Jáuregui <victor.diaz.jauregui@mi.unc.edu.ar>
2024-11-22 21:55:04 -0300

    Params del model pasados por env

--- main.js
+++ main.js
@@ -40,12 +40,17 @@ const generateTextFromInput = async () => {
       frames: ['🌑', '🌒', '🌓', '🌔', '🌕', '🌖', '🌗', '🌘'],
     }).start();
 
+    const model = process.env.MODEL || 'mistralai/Mixtral-8x7B-Instruct-v0.1';
+    const max_tokens = parseInt(process.env.MAX_TOKENS || '1000');
+    const temperature = parseFloat(process.env.TEMPERATURE || '0.1');
+    const seed = parseInt(process.env.SEED || '42');
+
     const response = await hf.chatCompletion({
-      model: 'mistralai/Mixtral-8x7B-Instruct-v0.1',
+      model,
       messages: conversationHistory,
-      max_tokens: 1000,
-      temperature: 0.1,
-      seed: 42,
+      max_tokens,
+      temperature,
+      seed,
     });
 
     load.stop();
