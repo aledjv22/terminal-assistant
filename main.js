2d501be6e5b60c48f8791c0b01ede5cadd35ae44
Victor Alejandro Díaz Jáuregui <victor.diaz.jauregui@mi.unc.edu.ar>
2024-11-24 21:07:38 -0300

    Mensajes importados

--- main.js
+++ main.js
@@ -9,6 +9,13 @@ import {
   temperature,
   seed,
 } from './src/config.js';
+import {
+  showUserInput,
+  showAssistantMessage,
+  showExitMessage,
+  showTokenError,
+  showMargins,
+} from './src/messages.js';
 
 const rl = readline.createInterface({
   input: process.stdin,
@@ -21,10 +28,7 @@ const hasToken = () => hf.accessToken !== '';
 
 const generateTextFromInput = async () => {
   if (!hasToken()) {
-    console.log(
-      chalk.bgYellow('No se ha encontrado un token de Hugging Face.'),
-    );
-    console.log(chalk.bgYellow('Por favor, revisa el archivo .env'));
+    showTokenError();
     rl.close();
     return;
   }
@@ -34,10 +38,10 @@ const generateTextFromInput = async () => {
     readline.moveCursor(process.stdout, 0, -(numLines + 1));
     readline.clearScreenDown(process.stdout);
 
-    console.log(chalk.bgBlue.black(chalk.underline.bold('Tu:'), input));
+    showUserInput(input);
 
     if (input.toLowerCase() === 'exit' || input.toLowerCase() === 'salir') {
-      console.log(chalk.bgYellow('¡Hasta luego! ^-^'));
+      showExitMessage();
       rl.close();
       return;
     }
@@ -59,14 +63,9 @@ const generateTextFromInput = async () => {
     const assistantMessage = response.choices[0].message.content;
     conversationHistory.push({ role: 'assistant', content: assistantMessage });
 
-    console.log(
-      chalk
-        .bgHex('#49c156')
-        .black(chalk.underline.bold('\nAsistente:'), assistantMessage),
-    );
+    showAssistantMessage(assistantMessage);
 
-    console.log(chalk.magenta('=========================================='));
-    console.log(chalk.magenta('=========================================='));
+    showMargins();
 
     generateTextFromInput();
   });
